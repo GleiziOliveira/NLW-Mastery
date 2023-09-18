07b5b2809d2253e5657a42e6626479033421e7c9
Gleizi Oliveira <105182020+GleiziOliveira@users.noreply.github.com>
2023-09-18 13:04:06 -0300

    Update get-all-prompts.ts

--- upload-ai-api/src/routes/get-all-prompts.ts
+++ upload-ai-api/src/routes/get-all-prompts.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 
 export async function getAllPromptsRoute(app: FastifyInstance){
-  app.get("http://localhost:3333/prompt", async () => {
+  app.get("/prompt", async () => {
     const prompts = await prisma.prompt.findMany()
 
     return prompts
